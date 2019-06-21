1a52cb38532d5807e575858f31859a160b334086
phanny.ngoun@gmail.com <**Hello123>
2019-06-21 18:19:52 +0700

    ''

--- server/app/utils/auth/SilhouetteModule.scala
+++ server/app/utils/auth/SilhouetteModule.scala
@@ -2,30 +2,30 @@ package utils.auth
 
 import com.dream.mytask.models.User
 import com.dream.mytask.services.{SysAuthInfoRepository, UserService, UserServiceImpl}
+import com.google.inject.Provides
 import com.google.inject.name.Named
-import com.google.inject.{AbstractModule, Provides}
+import com.mohiva.play.silhouette.api._
 import com.mohiva.play.silhouette.api.actions.{SecuredErrorHandler, UnsecuredErrorHandler}
 import com.mohiva.play.silhouette.api.crypto._
 import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
 import com.mohiva.play.silhouette.api.services._
 import com.mohiva.play.silhouette.api.util._
-import com.mohiva.play.silhouette.api._
 import com.mohiva.play.silhouette.crypto.{JcaCrypter, JcaCrypterSettings, JcaSigner, JcaSignerSettings}
 import com.mohiva.play.silhouette.impl.authenticators._
 import com.mohiva.play.silhouette.impl.providers._
 import com.mohiva.play.silhouette.impl.util._
 import com.mohiva.play.silhouette.password.{BCryptPasswordHasher, BCryptSha256PasswordHasher}
-import com.mohiva.play.silhouette.persistence.daos.{DelegableAuthInfoDAO, InMemoryAuthInfoDAO}
+import com.mohiva.play.silhouette.persistence.daos.DelegableAuthInfoDAO
 import com.mohiva.play.silhouette.persistence.repositories.DelegableAuthInfoRepository
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
-
-import scala.concurrent.ExecutionContext.Implicits.global
 import net.codingwell.scalaguice.ScalaModule
 import play.api.Configuration
 import play.api.libs.concurrent.AkkaGuiceSupport
 import play.api.libs.ws.WSClient
 import play.api.mvc.CookieHeaderEncoding
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait DefaultEnv extends Env {
   type I = User
@@ -42,8 +42,8 @@ class SilhouetteModule extends ScalaModule with AkkaGuiceSupport {
   override def configure(): Unit = {
     bind[UserService].to[UserServiceImpl]
     bind[EventBus].toInstance(EventBus())
-//    bind[UnsecuredErrorHandler].to[CustomUnsecuredErrorHandler]
-//    bind[SecuredErrorHandler].to[CustomSecuredErrorHandler]
+    bind[UnsecuredErrorHandler].to[CustomUnsecuredErrorHandler]
+    bind[SecuredErrorHandler].to[CustomSecuredErrorHandler]
     bind[IDGenerator].toInstance(new SecureRandomIDGenerator())
     bind[PasswordHasher].toInstance(new BCryptPasswordHasher)
     bind[FingerprintGenerator].toInstance(new DefaultFingerprintGenerator(false))
@@ -51,8 +51,8 @@ class SilhouetteModule extends ScalaModule with AkkaGuiceSupport {
     bind[Clock].toInstance(Clock())
     bind[java.time.Clock].toInstance(java.time.Clock.systemUTC())
     bind[Silhouette[DefaultEnv]].to[SilhouetteProvider[DefaultEnv]]
-    bind[Silhouette[AuthEnv]].to[SilhouetteProvider[AuthEnv]]
-//    bind[LdapUtil].to[LdapUtilImpl]
+//    bind[Silhouette[AuthEnv]].to[SilhouetteProvider[AuthEnv]]
+    //    bind[LdapUtil].to[LdapUtilImpl]
     bind[DelegableAuthInfoDAO[PasswordInfo]].to[SysAuthInfoRepository]
   }
 
@@ -61,9 +61,9 @@ class SilhouetteModule extends ScalaModule with AkkaGuiceSupport {
 
   @Provides
   def provideCookieAuthenticatorEnvironment(
-                          userService: UserService,
-                          authenticatorService: AuthenticatorService[CookieAuthenticator],
-                          eventBus: EventBus): Environment[DefaultEnv] = {
+    userService: UserService,
+    authenticatorService: AuthenticatorService[CookieAuthenticator],
+    eventBus: EventBus): Environment[DefaultEnv] = {
 
     Environment[DefaultEnv](
       userService,
@@ -73,29 +73,29 @@ class SilhouetteModule extends ScalaModule with AkkaGuiceSupport {
     )
   }
 
-  @Provides
-  def provideJWTAuthenticatorEnvironment(
-                                          userService: UserService,
-                                          authenticatorService: AuthenticatorService[JWTAuthenticator],
-                                          eventBus: EventBus): Environment[AuthEnv] = {
-
-    Environment[AuthEnv](
-      userService,
-      authenticatorService,
-      Seq(),
-      eventBus
-    )
-  }
+  //  @Provides
+  //  def provideJWTAuthenticatorEnvironment(
+  //                                          userService: UserService,
+  //                                          authenticatorService: AuthenticatorService[JWTAuthenticator],
+  //                                          eventBus: EventBus): Environment[AuthEnv] = {
+  //
+  //    Environment[AuthEnv](
+  //      userService,
+  //      authenticatorService,
+  //      Seq(),
+  //      eventBus
+  //    )
+  //  }
 
   @Provides
   def provideCookieAuthenticatorAuthenticatorService(
-                                                      @Named("authenticator-signer") signer: Signer,
-                                                      @Named("authenticator-crypter") crypter: Crypter,
-                                                      cookieHeaderEncoding: CookieHeaderEncoding,
-                                                      fingerprintGenerator: FingerprintGenerator,
-                                                      idGenerator: IDGenerator,
-                                                      configuration: Configuration,
-                                                      clock: Clock): AuthenticatorService[CookieAuthenticator] = {
+    @Named("authenticator-signer") signer: Signer,
+    @Named("authenticator-crypter") crypter: Crypter,
+    cookieHeaderEncoding: CookieHeaderEncoding,
+    fingerprintGenerator: FingerprintGenerator,
+    idGenerator: IDGenerator,
+    configuration: Configuration,
+    clock: Clock): AuthenticatorService[CookieAuthenticator] = {
 
     val config = configuration.underlying.as[CookieAuthenticatorSettings]("silhouette.CookieAuthenticator")
     val authenticatorEncoder = new CrypterAuthenticatorEncoder(crypter)
@@ -103,19 +103,20 @@ class SilhouetteModule extends ScalaModule with AkkaGuiceSupport {
     new CookieAuthenticatorService(config, None, signer, cookieHeaderEncoding, authenticatorEncoder, fingerprintGenerator, idGenerator, clock)
   }
 
-//  @Provides
-//  def provideJWTAuthenticatorAuthenticatorService(
-//                                   @Named("authenticator-crypter") crypter: Crypter,
-//                                   idGenerator: IDGenerator,
-//                                   configuration: Configuration,
-//                                   clock: Clock): AuthenticatorService[JWTAuthenticator] = {
-//    val config = configuration.underlying.as[JWTAuthenticatorSettings]("silhouette.JwtAuthenticator")
-//    val encoder = new CrypterAuthenticatorEncoder(crypter)
-//
-//    new JWTAuthenticatorService(config, None, encoder, idGenerator, clock)
-//  }
+  //  @Provides
+  //  def provideJWTAuthenticatorAuthenticatorService(
+  //    @Named("authenticator-crypter") crypter: Crypter,
+  //    idGenerator: IDGenerator,
+  //    configuration: Configuration,
+  //    clock: Clock): AuthenticatorService[JWTAuthenticator] = {
+  //    val config = configuration.underlying.as[JWTAuthenticatorSettings]("silhouette.JwtAuthenticator")
+  //    val encoder = new CrypterAuthenticatorEncoder(crypter)
+  //
+  //    new JWTAuthenticatorService(config, None, encoder, idGenerator, clock)
+  //  }
 
-  @Provides @Named("authenticator-signer")
+  @Provides
+  @Named("authenticator-signer")
   def provideAuthenticatorSigner(configuration: Configuration): Signer = {
     val config = configuration.underlying.as[JcaSignerSettings]("silhouette.CookieAuthenticator.signer")
 
@@ -123,7 +124,8 @@ class SilhouetteModule extends ScalaModule with AkkaGuiceSupport {
   }
 
 
-  @Provides @Named("authenticator-crypter")
+  @Provides
+  @Named("authenticator-crypter")
   def provideAuthenticatorCrypter(configuration: Configuration): Crypter = {
     val config = configuration.underlying.as[JcaCrypterSettings]("silhouette.CookieAuthenticator.crypter")
 
@@ -133,7 +135,7 @@ class SilhouetteModule extends ScalaModule with AkkaGuiceSupport {
 
   @Provides
   def provideAuthInfoRepository(
-                                 passwordInfoDAO: DelegableAuthInfoDAO[PasswordInfo]): AuthInfoRepository = {
+    passwordInfoDAO: DelegableAuthInfoDAO[PasswordInfo]): AuthInfoRepository = {
 
     new DelegableAuthInfoRepository(passwordInfoDAO)
   }
@@ -143,20 +145,20 @@ class SilhouetteModule extends ScalaModule with AkkaGuiceSupport {
     PasswordHasherRegistry(new BCryptSha256PasswordHasher(), Seq(new BCryptPasswordHasher()))
   }
 
-//  @Provides
-//  def provideLdapCredentialsProvider(
-//                                  authInfoRepository: AuthInfoRepository,
-//                                  passwordHasherRegistry: PasswordHasherRegistry,
-//                                  ldapUtil: LdapUtil
-//                                ): LdapCredentialsProvider = {
-//    new LdapCredentialsProvider(authInfoRepository, passwordHasherRegistry, ldapUtil)
-//  }
+//    @Provides
+//    def provideLdapCredentialsProvider(
+//                                    authInfoRepository: AuthInfoRepository,
+//                                    passwordHasherRegistry: PasswordHasherRegistry,
+//                                    ldapUtil: LdapUtil
+//                                  ): LdapCredentialsProvider = {
+//      new LdapCredentialsProvider(authInfoRepository, passwordHasherRegistry, ldapUtil)
+//    }
 
   @Provides
   def provideCredentialsProvider(
-                                  authInfoRepository: AuthInfoRepository,
-                                  passwordHasherRegistry: PasswordHasherRegistry
-                                ): CredentialsProvider = {
+    authInfoRepository: AuthInfoRepository,
+    passwordHasherRegistry: PasswordHasherRegistry
+  ): CredentialsProvider = {
     new CredentialsProvider(authInfoRepository, passwordHasherRegistry)
   }
 }
