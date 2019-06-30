0a74bd3caa5b61a7b7d113126f7e0860b79c0dc8
phanny phanny <phanny.ngoun@gmail.com>
2019-06-30 10:20:25 +0700

    ''

--- resources/ticket.sql
+++ resources/ticket.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Community v13.1.1 (64 bit)
+SQLyog Community v13.1.2 (64 bit)
 MySQL - 5.7.24 : Database - ticket
 *********************************************************************
 */
@@ -31,6 +31,11 @@ CREATE TABLE `account` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+/*Data for the table `account` */
+
+insert  into `account`(`id`,`name`,`full_name`,`sequence_nr`,`created_at`,`updated_at`,`active`) values 
+('94d7eaeb-d8d9-4ebd-b6a2-d324b5e64f9a','Testing','Testing',1,'2019-06-18 15:55:08','2019-06-18 15:55:08','');
+
 /*Table structure for table `flag` */
 
 DROP TABLE IF EXISTS `flag`;
@@ -42,6 +47,11 @@ CREATE TABLE `flag` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+/*Data for the table `flag` */
+
+insert  into `flag`(`id`,`code`,`val`) values 
+('1','workflow',7);
+
 /*Table structure for table `item` */
 
 DROP TABLE IF EXISTS `item`;
@@ -49,11 +59,18 @@ DROP TABLE IF EXISTS `item`;
 CREATE TABLE `item` (
   `id` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `workflow_id` varchar(100) DEFAULT NULL,
   `sequence_nr` bigint(20) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `item` */
+
+insert  into `item`(`id`,`name`,`description`,`workflow_id`,`sequence_nr`,`created_at`,`updated_at`) values 
+('99a132ae-b32b-4c43-b22f-ff3fd83aa215','Ticketing','test','42c9ae63-6767-4c1d-9b95-7cf4f71a4035',1,'2019-06-18 16:09:00','2019-06-18 16:09:00');
 
 /*Table structure for table `participant` */
 
@@ -72,6 +89,11 @@ CREATE TABLE `participant` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+/*Data for the table `participant` */
+
+insert  into `participant`(`id`,`account_id`,`team_id`,`department_id`,`property_id`,`sequence_nr`,`created_at`,`updated_at`,`active`) values 
+('b8275acf-2b4a-4910-9254-072030a07f4d','94d7eaeb-d8d9-4ebd-b6a2-d324b5e64f9a','94d7eaeb-d8d9-4ebd-b6a2-d324b5e64f9a','94d7eaeb-d8d9-4ebd-b6a2-d324b5e64f9a','94d7eaeb-d8d9-4ebd-b6a2-d324b5e64f9a','1','2019-06-18 15:55:30','2019-06-18 15:55:30','');
+
 /*Table structure for table `process_instance` */
 
 DROP TABLE IF EXISTS `process_instance`;
@@ -84,6 +106,8 @@ CREATE TABLE `process_instance` (
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `process_instance` */
 
 /*Table structure for table `workflow` */
 
@@ -98,6 +122,13 @@ CREATE TABLE `workflow` (
   `sequence_nr` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `workflow` */
+
+insert  into `workflow`(`id`,`name`,`created_at`,`updated_at`,`active`,`sequence_nr`) values 
+('42c9ae63-6767-4c1d-9b95-7cf4f71a4035','Ticket_xxx1','2019-06-18 15:56:55','2019-06-18 15:56:55','',1),
+('4cc499e6-5d87-46be-bb17-f743717a768f','Ticket','2019-06-18 16:01:49','2019-06-18 16:01:49','',1),
+('4d693b02-2757-4a25-9ba2-e830eb681f43','Ticket','2019-06-18 16:01:48','2019-06-18 16:01:48','',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
